34083398a10c976b8971d78205eb46bce9ebcada
Tim van Dijk <tim.vandijk96@gmail.com>
2016-05-20 11:41:55 +0200

    Worked towards parsing zone files

--- proj2_framework/dns/zone.py
+++ proj2_framework/dns/zone.py
@@ -10,15 +10,6 @@ zones or record sets.
 These classes are merely a suggestion, feel free to use something else.
 """
 
-#content = open('test.txt').read()
-#content = re.sub(re.compile(";.*?\n"), "\n", content)
-#content = re.sub(re.compile("\n\n*\n") , "\n", content)
-#Remove indentation
-#content = re.sub(re.compile("  * ") , " ", content) #Remove multiple spaces between words
-#content = re.sub(re.compile("\t\t*\t") , " ", content) #Remove tabs between words
-#content = re.sub(re.compile("\n *") , "\n", content) #Remove spaces at start of line
-#content = re.sub(re.compile("\n\t*") , "\n", content) #Remove tabs at start of line
-#print(content)
 class Catalog(object):
     """ A catalog of zones """
 
@@ -68,6 +59,24 @@ class Zone(object):
             print("An error has occured while reading the zone from file: " \
                 + str(filename) + " - " + str(e))
 
+    #Kan mooier met een dict, maar het werkt.
+    #Als je zin hebt om het te verbeteren, ga je gang.
+    def time_to_seconds_helper(self, timestring):
+        if (timestring[-1] in ['s', 'S']):
+            return int(timestring[:-1])
+        if (timestring[-1] in ['m', 'M']):
+            return 60 * int(timestring[:-1])
+        if (timestring[-1] in ['h', 'H']):
+            return 3600 * int(timestring[:-1])
+        if (timestring[-1] in ['w', 'W']):
+            return 86400 * int(timestring[:-1])
+        if (timestring[-1] in ['d', 'D']):
+            return 604800 * int(timestring[:-1])
+        return 0
+
+    def time_to_seconds(self, timestring):
+        return sum(map(time_to_seconds_helper, re.findall(re.compile("\d+\w"), timestring)))
+
     def parse_and_load(self, content):
         content = re.sub(re.compile(";.*?\n"), "\n", content) #Remove comments
         content = re.sub(re.compile("\n\n*\n") , "\n", content) #Remove whitespaces
@@ -75,4 +84,17 @@ class Zone(object):
         content = re.sub(re.compile("\t\t*\t") , " ", content) #Remove tabs between words
         content = re.sub(re.compile("\n *") , "\n", content) #Remove spaces at start of line
         content = re.sub(re.compile("\n\t*") , "\n", content) #Remove tabs at start of line
+        content = re.compile(r'\(.*?\)', re.DOTALL)\
+        .sub(lambda x: x.group().replace('\n', ''), content) #Remove newlines between ()
+
+        ttl = None
+        origin = None
+
+        for line in content:
+            if line[:4] == "$TTL":
+                ttl = time_to_seconds(strip(line[4:]))
+            elif line[:7] == "$ORIGIN":
+                origin = strip(line[7:])
+            elif "IN SOA" in line:
+                pass
 
