31d1dff32a3e8965f39e8954d14708de625c3d69
W-M-T <wgitcommit@gmail.com>
2016-05-30 12:57:10 +0200

    Large refactoring of resolver

--- proj2_s4499115_s4477073/dns/resolver.py
+++ proj2_s4499115_s4477073/dns/resolver.py
@@ -35,41 +35,42 @@ class Resolver(object):
         self.ttl = ttl if ttl > 0 else 0 #Deze check is niet nodig voor de resolver gemaakt via de server, maar wel voor de resolver gemaakt door de client
         if caching:
             self.cache = RecordCache()
-        self.identifier = random.randint(0, 65535)
         self.nameservers = nameservers
         if use_rs:
             self.nameservers += dns.consts.ROOT_SERVERS
         
-    def send_query(self, query, servers):
-        """ Send query to each server in servers
+    def ask_server(self, query, server):
+        """ Send query to a server
 
         Args: 
-            query (Message): the query that is to be send
-            servers ([str]): IP addresses of the server that the query must be sent to
+            query (Message): the query that is to be sent
+            server (str): IP address of the server that the query must be sent to
         
         Returns:
             responses ([Message]): the responses received converted to Messages
         """
-        responses = []
-        for server in servers:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.settimeout(self.timeout)
-            try:
-                sock.sendto(query.to_bytes(), (server, 53))
-                data = sock.recv(1024)
-                response = dns.message.Message.from_bytes(data)
-                if response.header.ident != query.header.ident:
-                    continue
-
-                responses.append(response)
-                if self.caching:
-                    for record in response.additionals + response.answers + response.authorities:
+        response = None
+        
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(self.timeout)
+        try:
+            sock.sendto(query.to_bytes(), (server, 53))
+            data = sock.recv(1024)
+            response = dns.message.Message.from_bytes(data)
+            if response.header.ident != query.header.ident:
+                return None
+            
+            if self.caching:
+                for record in response.additionals + response.answers + response.authorities:
+                    if record.type == Type.A or record.type == Type.CNAME:
                         record.ttl = self.ttl
                         record.timestamp = int(time.time())
                         self.cache.add_record(record)
-            except socket.timeout:
-                pass
-        return responses
+                    
+        except socket.timeout:
+            pass
+        
+        return response
 
     def save_cache(self):
         """ Save the cache if appropriate """
@@ -101,14 +102,14 @@ class Resolver(object):
             ipaddrlist ([str]): list of IP addresses of the hostname 
 
         """
-        aliaslist = []
-        ipaddrlist = []
-        hints = self.nameservers
+        
 
+        #Check if the hostname is valid
         valid = self.is_valid_hostname(hostname)
         if not valid:
             return hostname, [], []
 
+        #Check if the information is in the cache
         if self.caching:   		
             for alias in self.cache.lookup(hostname, Type.CNAME, Class.IN):
                 aliaslist.append(alias.rdata.data)
@@ -116,40 +117,41 @@ class Resolver(object):
             for address in self.cache.lookup(hostname, Type.A, Class.IN):
                 ipaddrlist.append(address.rdata.data)
 
-        if ipaddrlist:
-            return hostname, aliaslist, ipaddrlist
+            if ipaddrlist:
+                return hostname, aliaslist, ipaddrlist
 
-        #Send them queries until one returns a response.
-        identifier = (self.identifier + random.randint(1,2048)) % 25535
-        question = dns.message.Question(hostname, Type.A, Class.IN)
-        header = dns.message.Header(identifier, 0, 1, 0, 0, 0)
-        header.qr = 0
-        header.opcode = 0
-        header.rd = 1
-        query = dns.message.Message(header, [question])
+        #Do the recursive algorithm
+        aliaslist = []
+        ipaddrlist = []
+        hints = self.nameservers
         
         while hints:
-            responses = self.send_query(query, [hints[0]])
+            hint = hints[0]
             hints = hints[1:]
-            while responses == [] and hints != []:
-            	responses = self.send_query(query, [hints[0]])
-                hints = hints[1:]
 
-            if responses is None:
-                return hostname, [], []
+            identifier = randint(0, 65535)
+            
+            question = dns.message.Question(hostname, Type.A, Class.IN)
+            header = dns.message.Header(identifier, 0, 1, 0, 0, 0)
+            header.qr = 0
+            header.opcode = 0
+            header.rd = 0
+            query = dns.message.Message(header, [question])
+  
+            response = self.ask_server(query, hint)
+
+            if response == None:
+                continue
 
             #Analyze the response
-            for response in responses:
+            for answers in response.answers:
                 print("dit zijn de antwoorden in deze response")
-                print(response.answers)
-                for answer in response.answers:
-                    if answer.type_ == Type.CNAME and (answer.name == hostname or answer.name in aliaslist):
-                        if answer.rdata.data not in aliases:
-                            aliaslist.append(answer.rdata.data)
-                for answer in response.answers:
-                    print(answer.rdata.data)
-                    if answer.type_ == Type.A and (answer.name == hostname or answer.name in aliaslist):  
-                        ipaddrlist.append(answer.rdata.data)
+                if answer.type_ == Type.CNAME and (answer.name == hostname or answer.name in aliaslist):
+                    if answer.rdata.data not in aliases:
+                        aliaslist.append(answer.rdata.data)
+                print(answer.rdata.data)
+                if answer.type_ == Type.A and (answer.name == hostname or answer.name in aliaslist):  
+                    ipaddrlist.append(answer.rdata.data)
                 
                 for additional in response.additionals:
                     if additional.type_ == Type.CNAME and (additional.name == hostname or additional.name in aliaslist):
